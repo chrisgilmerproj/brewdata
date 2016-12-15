d4160a56ddc7b73b7095f8bd89e488688da65978
Chris Gilmer <chris.gilmer@gmail.com>
2016-12-15 14:17:54 -0800

    Unicode hops scraper

--- scraper/spiders/hops_spider.py
+++ scraper/spiders/hops_spider.py
@@ -4,15 +4,15 @@ from scraper.items import HopsItem
 
 
 class HopslistSpider(scrapy.Spider):
-    name = "hopslist"
-    allowed_domains = ["hopslist.com"]
+    name = u"hopslist"
+    allowed_domains = [u"hopslist.com"]
     start_urls = (
-        'http://www.hopslist.com/hops/',
+        u'http://www.hopslist.com/hops/',
     )
 
     def parse(self, response):
-        # for href in response.xpath("//ul[@class='category-module']/li/h6/a/@href").extract():  # nopep8
-        for href in response.xpath("//ul[@class='display-posts-listing']/li/a/@href").extract():  # nopep8
+        # for href in response.xpath(u"//ul[@class='category-module']/li/h6/a/@href").extract():  # nopep8
+        for href in response.xpath(u"//ul[@class='display-posts-listing']/li/a/@href").extract():  # nopep8
             url = response.urljoin(href)
             yield scrapy.Request(url, callback=self.parse_hops_contents)
 
@@ -20,20 +20,20 @@ class HopslistSpider(scrapy.Spider):
         item = HopsItem()
 
         item[u'source'] = response.url
-        item[u'source_id'] = item['source'].split('/')[-2]
+        item[u'source_id'] = item[u'source'].split(u'/')[-2]
 
-        item[u'component'] = item['source'].split('/')[-3]
-        name = response.xpath("//h1[@class='entry-title']/text()").extract()  # nopep8
-        item[u'name'] = '_'.join(name)
-        for entry in response.xpath('//table/tbody/tr'):
-            category = entry.xpath('td[1]/text()').extract()
-            data = entry.xpath('td[2]/text()').extract()
-            cat_safe = ' '.join([c.strip() for c in category]).lower().strip()
-            data_safe = ' '.join([d.strip() for d in data])
+        item[u'component'] = item[u'source'].split(u'/')[-3]
+        name = response.xpath(u"//h1[@class='entry-title']/text()").extract()  # nopep8
+        item[u'name'] = u'_'.join(name)
+        for entry in response.xpath(u'//table/tbody/tr'):
+            category = entry.xpath(u'td[1]/text()').extract()
+            data = entry.xpath(u'td[2]/text()').extract()
+            cat_safe = u' '.join([c.strip() for c in category]).lower().strip()
+            data_safe = u' '.join([d.strip() for d in data])
             if cat_safe:
-                cat_safe = cat_safe.replace('?', '')
-                cat_safe = cat_safe.replace(' ', '_')
-                cat_safe = cat_safe.replace(' ', '-')
+                cat_safe = cat_safe.replace(u'?', u'')
+                cat_safe = cat_safe.replace(u' ', u'_')
+                cat_safe = cat_safe.replace(u' ', u'-')
                 if u'humulone' in cat_safe:
                     cat_safe = u'co_humulone_composition'
                 if cat_safe == u'east_of_harvest':
