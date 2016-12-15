b50286668fcdccdd6d045c23ed240aced5f27787
Chris Gilmer <chris.gilmer@gmail.com>
2016-12-15 14:18:58 -0800

    Unicode cereals scraper

--- scraper/spiders/cereals_spider.py
+++ scraper/spiders/cereals_spider.py
@@ -4,14 +4,14 @@ from scraper.items import CerealsItem
 
 
 class BeerSmithSpider(scrapy.Spider):
-    name = "beersmith"
-    allowed_domains = ["beersmith.com"]
+    name = u"beersmith"
+    allowed_domains = [u"beersmith.com"]
     start_urls = (
-        'http://beersmith.com/grain-list/',
+        u'http://beersmith.com/grain-list/',
     )
 
     def parse(self, response):
-        for href in response.xpath('//table[@class="ms-list4-main"]/tbody/tr/td/a/@href').extract():  # nopep8
+        for href in response.xpath(u'//table[@class="ms-list4-main"]/tbody/tr/td/a/@href').extract():  # nopep8
             url = response.urljoin(href)
             yield scrapy.Request(url, callback=self.parse_cereals_contents)
 
@@ -19,23 +19,23 @@ class BeerSmithSpider(scrapy.Spider):
         item = CerealsItem()
 
         item[u'source'] = response.url
-        source_id = item['source'].split('/')[-1].split('.')[0].split('_')[-1]
+        source_id = item[u'source'].split(u'/')[-1].split(u'.')[0].split(u'_')[-1]
         item[u'source_id'] = source_id
 
-        name = response.xpath('//h2/text()').extract()[0]
+        name = response.xpath(u'//h2/text()').extract()[0]
         item[u'name'] = name
-        notes = response.xpath("//body/center/table/tbody/tr/td/text()").extract()  # nopep8
-        item[u'notes'] = ' '.join(notes).strip()
+        notes = response.xpath(u"//body/center/table/tbody/tr/td/text()").extract()  # nopep8
+        item[u'notes'] = u' '.join(notes).strip()
 
-        for entry in response.xpath('//table/tbody/tr/td/table/tbody/tr/td'):
-            category = entry.xpath('b/text()').extract()
-            data = entry.xpath('text()').extract()
-            cat_safe = ' '.join([c.strip() for c in category]).lower().strip()
-            data_safe = ' '.join([d.strip() for d in data])
+        for entry in response.xpath(u'//table/tbody/tr/td/table/tbody/tr/td'):
+            category = entry.xpath(u'b/text()').extract()
+            data = entry.xpath(u'text()').extract()
+            cat_safe = u' '.join([c.strip() for c in category]).lower().strip()
+            data_safe = u' '.join([d.strip() for d in data])
             if cat_safe:
                 cat_safe = cat_safe[:-1]
-                cat_safe = cat_safe.replace('/', '_')
-                cat_safe = cat_safe.replace(' ', '_')
+                cat_safe = cat_safe.replace(u'/', u'_')
+                cat_safe = cat_safe.replace(u' ', u'_')
                 if cat_safe == u'type':
                     cat_safe = u'cereal_type'
                 item[cat_safe] = data_safe
