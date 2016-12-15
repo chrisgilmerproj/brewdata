f5ffe484c751e7b635ca29e046b696000e0d8e2d
Chris Gilmer <chris.gilmer@gmail.com>
2016-12-15 14:31:32 -0800

    Unicode the pipelines

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -30,19 +30,20 @@ class CerealsPipeline(object):
     def process_item(self, item, spider):
         if not isinstance(item, CerealsItem):
             return item
-        filename = item['name'].lower().replace(" ", "_")
-        filename = filename.replace(",", "")
-        filename = filename.replace("(", "")
-        filename = filename.replace(")", "")
-        filename = filename.replace("/", "_")
-        filename = filename.replace("-", "_")
-        filename = filename.replace("___", "_")
-        filename = filename.replace("__", "_")
-        filename = filename.replace("_-", "_")
-        filename = "{}.json".format(filename)
+        filename = item[u'name'].lower()
+        filename = filename.replace(u" ", u"_")
+        filename = filename.replace(u",", u"")
+        filename = filename.replace(u"(", u"")
+        filename = filename.replace(u")", u"")
+        filename = filename.replace(u"/", u"_")
+        filename = filename.replace(u"-", u"_")
+        filename = filename.replace(u"___", u"_")
+        filename = filename.replace(u"__", u"_")
+        filename = filename.replace(u"_-", u"_")
+        filename = u"{}.json".format(filename)
         filepath = os.path.join(os.path.abspath(CEREALS_DIR), filename)
-        item[u'color'] = float(item['color'][:-4])
-        item[u'ppg'] = round((float(item['potential'][:-3]) - 1.0) * 1000, 1)
+        item[u'color'] = float(item[u'color'][:-4])
+        item[u'ppg'] = round((float(item[u'potential'][:-3]) - 1.0) * 1000, 1)
         with open(filepath, 'wb') as f:
             line = json.dumps(dict(item))
             f.write(line)
@@ -54,15 +55,15 @@ class HopsPipeline(object):
     def process_item(self, item, spider):
         if not isinstance(item, HopsItem):
             return item
-        filename = item['source_id'].lower().replace(" ", "_")
-        filename = filename.replace("(", "")
-        filename = filename.replace(")", "")
-        filename = filename.replace("'", "")
-        filename = filename.replace("-", "_")
-        filename = "{}.json".format(filename)
+        filename = item[u'source_id'].lower().replace(u" ", u"_")
+        filename = filename.replace(u"(", u"")
+        filename = filename.replace(u")", u"")
+        filename = filename.replace(u"'", u"")
+        filename = filename.replace(u"-", u"_")
+        filename = u"{}.json".format(filename)
         filepath = os.path.join(os.path.abspath(HOPS_DIR), filename)
         if item[u'alpha_acid_composition']:
-            item[u'percent_alpha_acids'] = round(float(item['alpha_acid_composition'].split('-')[0].split('%')[0]) / 100., 3)  # noqa
+            item[u'percent_alpha_acids'] = round(float(item[u'alpha_acid_composition'].split(u'-')[0].split(u'%')[0]) / 100., 3)  # noqa
         with open(filepath, 'wb') as f:
             line = json.dumps(dict(item))
             f.write(line)
@@ -71,14 +72,14 @@ class HopsPipeline(object):
 
 class YeastPipeline(object):
     ATTENUATION = {
-        'NA': '0%',
-        '-': '0%',
-        'Low': '72%',
-        'Medium': '75%',
-        'Med-High': '76-77%',
-        'Medium-High': '76-77%',
-        'High': '78%',
-        'Very High': '80%',
+        u'NA': u'0%',
+        u'-': u'0%',
+        u'Low': u'72%',
+        u'Medium': u'75%',
+        u'Med-High': u'76-77%',
+        u'Medium-High': u'76-77%',
+        u'High': u'78%',
+        u'Very High': u'80%',
     }
 
     def process_item(self, item, spider):
@@ -86,25 +87,26 @@ class YeastPipeline(object):
             return item
         if item[u'attenuation'] in self.ATTENUATION:
             item[u'attenuation'] = self.ATTENUATION[item[u'attenuation']]
-        item[u'attenuation'] = item[u'attenuation'].replace("<", "")
-        item[u'attenuation'] = item[u'attenuation'].replace(">", "")
-        item[u'attenuation'] = item[u'attenuation'].replace("%", "")
-        item[u'attenuation'] = item[u'attenuation'].split('-')
+        item[u'attenuation'] = item[u'attenuation'].replace(u"<", u"")
+        item[u'attenuation'] = item[u'attenuation'].replace(u">", u"")
+        item[u'attenuation'] = item[u'attenuation'].replace(u"%", u"")
+        item[u'attenuation'] = item[u'attenuation'].split(u'-')
         item[u'attenuation'] = [float(att) / 100. for att in item[u'attenuation']]  # noqa
         item[u'percent_attenuation'] = sum(item[u'attenuation']) / len(item[u'attenuation'])  # noqa
 
+        # Remove en dash
         item[u'name'] = item[u'name'].replace('\u2013', '-')
 
-        if 'yeast_id' in item:
-            identifier = item['yeast_id'].lower()
+        if u'yeast_id' in item:
+            identifier = item[u'yeast_id'].lower()
         else:
-            identifier = item['name'].lower()
-        filename = '{}_{}'.format(item['manufacturer'].lower(),
-                                  identifier)
-        filename = filename.replace(" ", "_")
-        filename = filename.replace("-", "_")
-        filename = filename.replace("/", "_")
-        filename = "{}.json".format(filename)
+            identifier = item[u'name'].lower()
+        filename = u'{}_{}'.format(item[u'manufacturer'].lower(),
+                                   identifier)
+        filename = filename.replace(u" ", u"_")
+        filename = filename.replace(u"-", u"_")
+        filename = filename.replace(u"/", u"_")
+        filename = u"{}.json".format(filename)
         filepath = os.path.join(os.path.abspath(YEAST_DIR), filename)
         with open(filepath, 'wb') as f:
             line = json.dumps(dict(item))
