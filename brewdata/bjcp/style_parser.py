015deed4cbbf09b86cd8316b0adf263c79f49b05
Chris Gilmer <chris.gilmer@gmail.com>
2017-09-23 20:19:11 -0700

    Update parser to get historical beers

--- brewdata/bjcp/style_parser.py
+++ brewdata/bjcp/style_parser.py
@@ -14,9 +14,10 @@ def main():
     categories = {}
     style_map = {}
 
-    filename = b'2015_Styles.csv'
-    with open(filename, b'rb') as fileobj:
+    filename = '2015_Styles.csv'
+    with open(filename, 'r') as fileobj:
         reader = csv.reader(fileobj)
+        historical_subcat = 64
         for row in reader:
             rownum = row[0]
             category = row[1]
@@ -41,18 +42,23 @@ def main():
             smoke_roast_wood_fruit = row[20]
             cat_sub_2008 = row[22]
 
+            historical = 'Historical Beer:'
+            if style.startswith(historical):
+                category = '27'
+                # style = style[len(historical):]
+
             if rownum and category:
                 # Split up specialty IPAs
-                if not og and style and b'Specialty' not in style:
-                    cat_num, cat_name = style.split(b'. ')
-                    if cat_name == b'IPA':
-                        cat_name = b'India Pale Ale'
+                if not og and style and 'Specialty' not in style:
+                    cat_num, cat_name = style.split('. ')
+                    if cat_name == 'IPA':
+                        cat_name = 'India Pale Ale'
                     if cat_num not in categories:
                         categories[cat_num] = string.capwords(cat_name)
 
                 if og:
-                    cat = b''
-                    subcat = b''
+                    cat = ''
+                    subcat = ''
 
                     if category[-2].isalpha():
                         subcat = category[-2:]
@@ -60,24 +66,39 @@ def main():
                     elif category[-1].isalpha():
                         subcat = category[-1:]
                         cat = category[:-1]
-                    cat = int(cat)
+
+                    # Handle historical beers
+                    if cat:
+                        cat = int(cat)
+                    else:
+                        cat = category
+
+                    if not subcat:
+                        historical_subcat += 1
+                        subcat = chr(historical_subcat)
 
                     # Create the dictionary
                     if cat not in style_map:
                         style_map[cat] = {}
 
-                    style_name = b''
-                    if b'.' in style:
-                        subcat, style_name = style.split(b'. ')
+                    style_name = ''
+                    if '.' in style:
+                        subcat, style_name = style.split('. ')
                         if not subcat and subcat == category[-1]:
                             cat = category[:-1]
+                    if not style_name:
+                        style_name = style
 
-                    og_vals = og.split(b'-')
-                    fg_vals = fg.split(b'-')
-                    abv_vals = abv.split(b'-')
-                    ibu_vals = ibu.split(b'-')
-                    color_vals = color_srm.split(b'-')
-                    if og_vals[0] == b'Varies':
+                    og_vals = og.split('-')
+                    fg_vals = fg.split('-')
+                    abv_vals = abv.split('-')
+                    ibu_vals = ibu.split('-')
+                    if '-' in color_srm:
+                        color_vals = color_srm.split('-')
+                    else:
+                        # Gose
+                        color_vals = color_srm.split('.')
+                    if og_vals[0] == 'Varies':
                         og_vals = []
                         fg_vals = []
                         abv_vals = []
@@ -88,35 +109,35 @@ def main():
                         if float(fg_vals[1]) > 1.015:
                             fg_vals[1] = str(1.0 + float(fg_vals[1]) / 1000.)
                     info = {
-                        b'category': cat,
-                        b'subcategory': subcat.strip(),
-                        b'style': style_name.strip(),
-                        b'og': [float(o) for o in og_vals],
-                        b'fg': [float(f) for f in fg_vals],
-                        b'abv': [round(float(a) / 100.0, 3) for a in abv_vals],
-                        b'ibu': [float(i) for i in ibu_vals],
-                        b'color': [float(c) for c in color_vals],
-                        b'tags': tags,
-                        b'strength': strength,
-                        b'color_name': color_name,
-                        b'fermentation': fermentation,
-                        b'conditioning': conditioning,
-                        b'region_of_origin': region_of_origin,
-                        b'style_family': style_family,
-                        b'specialty_beer': specialty_beer,
-                        b'era': era,
-                        b'bitter_balance_malty': bitter_balance_malty,
-                        b'sour_hoppy_sweet': sour_hoppy_sweet,
-                        b'spice': spice,
-                        b'smoke_roast_wood_fruit': smoke_roast_wood_fruit,
-                        b'cat_sub_2008': cat_sub_2008,
+                        'category': cat,
+                        'subcategory': subcat.strip(),
+                        'style': style_name.strip(),
+                        'og': [float(o) for o in og_vals],
+                        'fg': [float(f) for f in fg_vals],
+                        'abv': [round(float(a) / 100.0, 3) for a in abv_vals],
+                        'ibu': [float(i) for i in ibu_vals],
+                        'color': [float(c) for c in color_vals],
+                        'tags': tags,
+                        'strength': strength,
+                        'color_name': color_name,
+                        'fermentation': fermentation,
+                        'conditioning': conditioning,
+                        'region_of_origin': region_of_origin,
+                        'style_family': style_family,
+                        'specialty_beer': specialty_beer,
+                        'era': era,
+                        'bitter_balance_malty': bitter_balance_malty,
+                        'sour_hoppy_sweet': sour_hoppy_sweet,
+                        'spice': spice,
+                        'smoke_roast_wood_fruit': smoke_roast_wood_fruit,
+                        'cat_sub_2008': cat_sub_2008,
                     }
                     style_map[cat][subcat] = info
 
-    with open(b'categories.json', b'w') as fileobj:
+    with open('categories.json', 'w') as fileobj:
         fileobj.write(json.dumps(categories))
 
-    with open(b'styles.json', b'w') as fileobj:
+    with open('styles.json', 'w') as fileobj:
         fileobj.write(json.dumps(style_map))
 
 
